fe703e9a9ac79862e069719c8ac90188394fbdc6
Utkarsh <r.utkarsh.0010@gmail.com>
2023-08-12 22:24:52 +0530

    install required packages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
