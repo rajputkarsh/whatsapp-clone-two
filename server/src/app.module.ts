f68f895f78bef16d1e6cd2322a829c73333473f8
Utkarsh <r.utkarsh.0010@gmail.com>
2023-08-13 00:14:34 +0530

    update firebase configurations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [AuthModule, MessageModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.env` }),
+    AuthModule,
+    MessageModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
