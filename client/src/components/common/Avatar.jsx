0991f45e925a9ba3086a77e008181eb013885234
Utkarsh <r.utkarsh.0010@gmail.com>
2023-10-05 00:28:38 +0530

    save progress: partial feature completion

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaCamera } from 'react-icons/fa';
 
-function Avatar() {
-  return <div>Avatar</div>;
+function Avatar({type, image, setImage}) {
+
+  const [hover, setHover] = useState(false);
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+  const [contextMenuCoordinates, setContextMenuCoordinates] = useState({x: 0, y: 0});
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setIsContextMenuVisible(true);
+    setContextMenuCoordinates(() => ({x: e.pageX, y: e.pageY}));
+  }
+
+  return <>
+    <div className="flex items-center justify-center">
+        {
+          type === 'sm' && (
+            <div className="relative h-10 w-10">
+              <Image src={image} alt="avatar" className="rounded-full" fill />
+            </div>
+          )
+        }
+        {
+          type === 'lg' && (
+            <div className="relative h-14 w-14">
+              <Image src={image} alt="avatar" className="rounded-full" fill />
+            </div>
+          )
+        }
+      {
+        type === 'xl' && (
+          <div
+            className="relative cursor-pointer z-0"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onClick={(e) => showContextMenu(e)}
+          >
+            <div
+              onClick={(e) => showContextMenu(e)}
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center justify-center rounded-full flex-col text-center gap-2 ${hover ? "visible" : "hidden"}`}
+            >
+              <FaCamera
+                className="text-2xl"
+                id="context-opener"
+                onClick={(e) => showContextMenu(e)}
+              />
+              <span onClick={(e) => showContextMenu(e)}>Change <br /> Profile <br /> Photo</span>
+            </div>
+            <div className="h-60 w-60 flex items-center justify-center">
+              <Image src={image} alt="avatar" className="rounded-full" fill />
+            </div>
+          </div>
+        )
+      }
+    </div>
+  </>;
 }
 
 export default Avatar;
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import Input from '../components//common/Input';
 import { useStateProvider } from "../context/StateContext";
+import Avatar from "../components/common/Avatar";
 
 function register() {
-  const [{userInfo}, dispatch] = useStateProvider();
+  const [{ userInfo }, dispatch] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name || "");
+  const [about, setAbout] = useState("Hey There !");
+  const [image, setImage] = useState("/default_avatar.png");
+
   return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
-    <div className="flex items-center justify-ceenter gap-2">
+    <div className="flex items-center justify-center gap-2">
       <Image
         src="/whatsapp.gif"
         alt="chat"
@@ -14,10 +20,24 @@ function register() {
       />
       <span className="text-7xl">Let's Chat</span>
     </div>
-    <h2 className="text-xl">Create your Profile</h2>
-    <div className="flex-gap-6 mt-6">
+    <h2 className="text-2xl mt-8">Create your Profile</h2>
+    <div className="flex gap-6 mt-6">
       <div className="flex flex-col items-center justify-center mt-5 gap-6">
-        {userInfo?.name || "NAME"}
+        <Input
+          name="Display Name"
+          state={name}
+          setState={setName}
+          label={true}
+        />
+        <Input
+          name="About"
+          state={about}
+          setState={setAbout}
+          label={true}
+        />
+      </div>
+      <div>
+        <Avatar type="xl" image={image} />
       </div>
     </div>
   </div>;
